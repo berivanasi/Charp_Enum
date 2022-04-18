53c001f438dae68bd5376d40e58a5c8c2386146d
Berivan <83500587+berivanasi@users.noreply.github.com>
2022-04-19 00:54:30 +0300

    Charp-Enum
    Enum Örneği

---
+++ Enum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        //Aldığı değeri vermek için int ekledim
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)Kategoriler.Elektronikler);
+            Console.ReadKey();
+
+        }
+        //Önemli bir yeri vardır çünkü tekrar yazımları kodları engeller Hız ve verim kazandırır
+        enum Kategoriler
+        {
+            Elektronikler = 3,
+            Hırdavat=2,
+            Güzellik=4,
+            Giyim=5
+             }
+    }
+}
